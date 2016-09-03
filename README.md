2cecb8a7a3563eada0fe5cb759e48287143ab68e
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-09-03 18:33:27 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,486 @@
+nnash
+===========
+
+[![Build Status](https://secure.travis-ci.org/eljefedelrodeodeljefe/nnash.svg?branch=master)](http://travis-ci.org/eljefedelrodeodeljefe/nnash)
+[![Build Status](https://david-dm.org/eljefedelrodeodeljefe/nnash.svg)](https://david-dm.org/eljefedelrodeodeljefe/nnash)
+[![NPM version](https://badge.fury.io/js/nnash.svg)](http://badge.fury.io/js/nnash)
+
+[![NPM](https://nodei.co/npm/nnash.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/nnash/)
+
+# Simple and fast NodeJS internal caching.
+
+A simple caching module that has `set`, `get` and `delete` methods and works a little bit like memcached.  
+Keys can have a timeout (`ttl`) after which they expire and are deleted from the cache.  
+All keys are stored in a single object so the practical limit is at around 1m keys.
+
+# Install
+
+```bash
+  npm install nnash --save
+```
+
+Or just require the `node_cache.js` file to get the superclass
+
+# Examples:
+
+## Initialize (INIT):
+
+```js
+const Cache = require('nnash')
+const cache = new Cache(options)
+```
+
+### Options
+
+- `stdTTL`: *(default: `0`)* the standard ttl as number in seconds for every generated cache element.  
+`0` = unlimited
+- `checkperiod`: *(default: `600`)* The period in seconds, as a number, used for the automatic delete check interval.  
+`0` = no periodic check.
+- `errorOnMissing`: *(default: `false`)* en/disable throwing or passing an error to the callback if attempting to `.get` a missing or expired value.
+- `useClones`: *(default: `true`)* en/disable cloning of variables. If `true` you'll get a copy of the cached variable. If `false` you'll save and get just the reference.  
+**Note:** `true` is recommended, because it'll behave like a server-based caching. You should set `false` if you want to save complex variable types like functions, promises, regexp, ...
+
+```js
+const Cache = require('nnash')
+const cache = new Cache({ stdTTL: 100, checkperiod: 120 } );
+```
+
+## Store a key (SET):
+
+`cache.set( key, val, [ ttl ], [callback] )`
+
+Sets a `key` `value` pair. It is possible to define a `ttl` (in seconds).  
+Returns `true` on success.
+
+```js
+const obj = { my: 'Special', variable: 42 }
+cache.set('myKey', obj, function (err, success) {
+  if(!err && success) {
+    console.log(success)
+    // true
+    // ... do something ...
+  }
+})
+```
+
+> Note: If the key expires based on it's `ttl` it will be deleted entirely from the internal data object.
+
+**Since `1.0.0`**:  
+Callback is now optional. You can also use synchronous syntax.
+
+```js
+const obj = { my: 'Special', variable: 42 }
+const success = cache.set('myKey', obj, 10000 )
+// true
+```
+
+
+## Retrieve a key (GET):
+
+`cache.get(key, [callback])`
+
+Gets a saved value from the cache.
+Returns a `undefined` if not found or expired.
+If the value was found it returns an object with the `key` `value` pair.
+
+```js
+cache.get('myKey', function(err, value) {
+  if(!err) {
+    if(value == undefined) {
+      // key not found
+    } else {
+      console.log(value)
+      // { my: "Special', variable: 42 }
+      // ... do something ...
+    }
+  }
+})
+```
+
+**Since `1.0.0`**:  
+Callback is now optional. You can also use synchronous syntax.
+
+```js
+value = cache.get('myKey')
+if ( value == undefined ) {
+  // handle miss!
+}
+// { my: "Special', variable: 42 }
+```
+
+**Since `2.0.0`**:  
+
+The return format changed to a simple value and a `ENOTFOUND` error if not found *( as `callback( err )` or on sync call as result instance of `Error` )*.
+
+**Since `2.1.0`**:
+
+The return format changed to a simple value, but a due to discussion in #11 a miss shouldn't return an error.
+So after 2.1.0 a miss returns `undefined`.
+
+**Since `3.1.0`**
+`errorOnMissing` option added
+
+```js
+try{
+    value = cache.get('not-existing-key', true );
+} catch (err) {
+    // ENOTFOUND: Key `not-existing-key` not found
+}
+```
+
+## Get multiple keys (MGET):
+
+`cache.mget( [ key1, key2, ... ,keyn ], [callback] )`
+
+Gets multiple saved values from the cache.
+Returns an empty object `{}` if not found or expired.
+If the value was found it returns an object with the `key` `value` pair.
+
+```js
+cache.mget([ 'myKeyA', 'myKeyB' ], function (err, value) {
+  if (!err) {
+    console.log(value)
+    /*
+      {
+        "myKeyA": { my: 'Special', variable: 123 },
+        "myKeyB": { the: 'Glory', answer: 42 }
+      }
+    */
+    // ... do something ...
+
+});
+```
+
+**Since `1.0.0`**:  
+Callback is now optional. You can also use synchronous syntax.
+
+```js
+const value = cache.mget([ 'myKeyA', 'myKeyB' ])
+/*
+  {
+    "myKeyA": { my: "Special', variable: 123 },
+    "myKeyB": { the: "Glory', answer: 42 }
+  }
+*/
+```
+
+**Since `2.0.0`**:  
+
+The method for mget changed from `.get([ 'a', 'b' ] )` to `.mget( [ 'a', 'b' ])`
+
+## Delete a key (DEL):
+
+`cache.del(key, [callback])`
+
+Delete a key. Returns the number of deleted entries. A delete will never fail.
+
+```js
+cache.del('myKey', function( err, count ){
+  if( !err ){
+    console.log( count ); // 1
+    // ... do something ...
+  }
+});
+```
+
+**Since `1.0.0`**:  
+Callback is now optional. You can also use synchronous syntax.
+
+```js
+value = cache.del('A');
+// 1
+```
+
+## Delete multiple keys (MDEL):
+
+`cache.del( [ key1, key2, ... ,keyn ], [callback] )`
+
+Delete multiple keys. Returns the number of deleted entries. A delete will never fail.
+
+```js
+cache.del([ 'myKeyA', 'myKeyB' ], function (err, count) {
+  if (!err) {
+    console.log(count) // 2
+    // ... do something ...
+  }
+})
+```
+
+**Since `1.0.0`**:  
+Callback is now optional. You can also use synchronous syntax.
+
+```js
+value = cache.del('A');
+// 1
+
+value = cache.del([ 'B', 'C' ])
+// 2
+
+value = cache.del([ 'A', 'B', 'C', 'D' ])
+// 1 - because A, B and C not exists
+```
+
+## Change TTL (TTL):
+
+`cache.ttl( key, ttl, [callback] )`
+
+Redefine the ttl of a key. Returns true if the key has been found and changed. Otherwise returns false.  
+If the ttl-argument isn't passed the default-TTL will be used.
+
+```js
+const cache = new NodeCache({ stdTTL: 100 })
+cache.ttl('existendKey', 100, function (err, changed) {
+  if (!err) {
+    console.log( changed ); // true
+    // ... do something ...
+  }
+});
+
+cache.ttl('missingKey', 100, function (err, changed) {
+  if (!err) {
+    console.log(changed) // false
+    // ... do something ...
+  }
+});
+
+cache.ttl('existendKey', function (err, changed) {
+  if (!err) {
+    console.log(changed) // true
+    // ... do something ...
+  }
+})
+```
+
+## Get TTL (getTTL):
+
+`cache.getTTL(key, [callback])`
+
+Receive the ttl of a key.
+You will get:
+- `undefined` if the key does not exist
+- `0` if this key has no ttl
+- a timestamp in ms until the key expires
+
+```js
+const cache = new NodeCache({ stdTTL: 100 })
+
+// Date.now() = 1456000500000
+cache.set('ttlKey', 'MyExpireData')
+cache.set('noTtlKey', 0, 'NonExpireData')
+
+const ts = cache.getTTL('ttlKey')
+// ts wil be approximately 1456000600000
+
+cache.getTTL('ttlKey', function( err, ts ){
+  if (!err) {
+    // ts wil be approximately 1456000600000
+  }
+})
+// ts wil be approximately 1456000600000
+
+const ts = cache.getTTL('noTtlKey')
+// ts = 0
+
+const ts = cache.getTTL('unknownKey')
+// ts = undefined
+
+```
+
+## List keys (KEYS)
+
+`cache.keys( [callback] )`
+
+Returns an array of all existing keys.  
+
+```js
+// async
+cache.keys(function (err, mykeys) {
+  if (!err) {
+    console.log( mykeys )
+   // [ "all', "my', "keys', "foo', "bar" ]
+  }
+})
+
+// sync
+const mykeys = cache.keys()
+
+console.log( mykeys )
+// [ "all', "my', "keys', "foo', "bar" ]
+```
+
+## Statistics (STATS):
+
+`cache.getStats()`
+
+Returns the statistics.  
+
+```js
+cache.getStats()
+  /*
+    {
+      keys: 0,    // global key count
+      hits: 0,    // global hit count
+      misses: 0,  // global miss count
+      ksize: 0,   // global key size count
+      vsize: 0    // global value size count
+    }
+  */
+```
+
+## Flush all data (FLUSH):
+
+`cache.flushAll()`
+
+Flush all data.  
+
+```js
+cache.flushAll();
+cache.getStats();
+  /*
+    {
+      keys: 0,    // global key count
+      hits: 0,    // global hit count
+      misses: 0,  // global miss count
+      ksize: 0,   // global key size count
+      vsize: 0    // global value size count
+    }
+  */
+```
+
+## Close the cache:
+
+`cache.close()`
+
+This will clear the interval timeout which is set on check period option.
+
+```js
+cache.close()
+```
+
+# Events
+
+## set
+
+Fired when a key has been added or changed.
+You will get the `key` and the `value` as callback argument.
+
+```js
+cache.on('set', function( key, value ){
+  // ... do something ...  
+});
+```
+
+## del
+
+Fired when a key has been removed manually or due to expiry.
+You will get the `key` and the deleted `value` as callback arguments.
+
+```js
+cache.on('del', functionv(key, value) {
+  // ... do something ...  
+})
+```
+
+## expired
+
+Fired when a key expires.
+You will get the `key` and `value` as callback argument.
+
+```js
+cache.on('expired', function (key, value) {
+  // ... do something ...  
+})
+```
+
+## flush
+
+Fired when the cache has been flushed.
+
+```js
+cache.on('flush', function () {
+  // ... do something ...  
+})
+```
+
+## Benchmarks
+
+### Version 1.1.x
+
+After adding io.js to the travis test here are the benchmark results for set and get of 100000 elements.
+But be careful with this results, because it has been executed on travis machines, so it is not guaranteed, that it was executed on similar hardware.
+
+**node.js `0.10.36`**  
+SET: `324`ms ( `3.24`µs per item )  
+GET: `7956`ms ( `79.56`µs per item )   
+
+**node.js `0.12.0`**  
+SET: `432`ms ( `4.32`µs per item )  
+GET: `42767`ms ( `427.67`µs per item )   
+
+**io.js `v1.1.0`**  
+SET: `510`ms ( `5.1`µs per item )  
+GET: `1535`ms ( `15.35`µs per item )   
+
+### Version 2.0.x
+
+Again the same benchmarks by travis with version 2.0
+
+**node.js `0.6.21`**  
+SET: `786`ms ( `7.86`µs per item )  
+GET: `56`ms ( `0.56`µs per item )   
+
+**node.js `0.10.36`**  
+SET: `353`ms ( `3.53`µs per item )
+GET: `41`ms ( `0.41`µs per item )   
+
+**node.js `0.12.2`**  
+SET: `327`ms ( `3.27`µs per item )  
+GET: `32`ms ( `0.32`µs per item )   
+
+**io.js `v1.7.1`**  
+SET: `238`ms ( `2.38`µs per item )  
+GET: `34`ms ( `0.34`µs per item )  
+
+> As you can see the version 2.x will increase the GET performance up to 200x in node 0.10.x.
+This is possible because the memory allocation for the object returned by 1.x is very expensive.
+
+### Version 3.0.x
+
+*see [travis results](https://travis-ci.org/eljefedelrodeodeljefe/nnash/builds/64560503)*
+
+**node.js `0.6.21`**  
+SET: `786`ms ( `7.24`µs per item )  
+GET: `56`ms ( `1.14`µs per item )   
+
+**node.js `0.10.38`**  
+SET: `353`ms ( `5.41`µs per item )
+GET: `41`ms ( `1.23`µs per item )   
+
+**node.js `0.12.4`**  
+SET: `327`ms ( `4.63`µs per item )  
+GET: `32`ms ( `0.60`µs per item )   
+
+**io.js `v2.1.0`**  
+SET: `238`ms ( `4.06`µs per item )  
+GET: `34`ms ( `0.67`µs per item )
+
+> until the version 3.0.x the object cloning is included, so we lost a little bit of the performance
+
+### Version 3.1.x
+
+**node.js `v0.10.41`**  
+SET: `305ms`  ( `3.05µs` per item )  
+GET: `104ms`  ( `1.04µs` per item )
+
+**node.js `v0.12.9`**  
+SET: `337ms`  ( `3.37µs` per item )  
+GET: `167ms`  ( `1.67µs` per item )
+
+**node.js `v4.2.6`**  
+SET: `356ms`  ( `3.56µs` per item )  
+GET: `83ms`  ( `0.83µs` per item )
+
+## Compatibility
+
+
+# The MIT License (MIT)
---
+++ test/ncache.legacy.spec.js
@@ -0,0 +1,734 @@
+const test = require('tape')
+const _clone = require("lodash/clone")
+const VCache = require("../")
+
+let localCache = new VCache({
+  stdTTL: 0
+})
+
+let localCacheNoClone = new VCache({
+  stdTTL: 0,
+  useClones: false,
+  checkperiod: 0
+})
+
+let localCacheTTL = new VCache({
+  stdTTL: 0.3,
+  checkperiod: 0
+})
+
+localCache._killCheckPeriod()
+
+function randomString (length, withnumbers) {
+  var chars, i, randomstring, rnum, string_length
+  if (withnumbers == null) {
+    withnumbers = true
+  }
+  chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if (withnumbers) {
+    chars += "0123456789"
+  }
+  string_length = length || 5;
+  randomstring = ""
+  i = 0
+  while (i < string_length) {
+    rnum = Math.floor(Math.random() * chars.length)
+    randomstring += chars.substring(rnum, rnum + 1)
+    i++
+  }
+  return randomstring;
+};
+
+
+test('general', function (t) {
+  t.plan(27)
+  console.log("\nSTART GENERAL TEST: `" + VCache.version + ("` on node:`" + process.version + "`"))
+
+  const c = new VCache()
+
+  let n = 0
+  const start = _clone(c.getStats())
+  const value = randomString(100)
+  const value2 = randomString(100)
+  const key = randomString(10)
+
+  c.once("del", function (_key, _val) {
+    t.equal(_key, key)
+    t.equal(_val, value2)
+  })
+
+  c.set(key, value, 0, function (err, res) {
+    t.equal(err, null)
+    n++
+    t.equal(1, c.getStats().keys - start.keys)
+
+    c.get(key, function (err, res) {
+      n++
+      t.equal(value, res)
+    })
+
+    c.keys(function (err, res) {
+      n++
+      const pred = [key]
+      t.deepEqual(pred, res)
+    })
+
+    c.get("yxx", function (err, res) {
+      n++
+      t.equal(err, null)
+      t.equal(res, undefined)
+    })
+
+    const errorHandlerCallback = function (err, res) {
+      n++
+      t.equal(err.name, "ENOTFOUND")
+      // t.equal(err.message, "Key `xxx` not found")
+    }
+
+    c.get("xxx", errorHandlerCallback, true)
+
+    try {
+      c.get("xxy", true)
+    } catch (error) {
+      const err = error
+      n++
+      t.equal(err.name, "ENOTFOUND")
+      // t.equal(err.message, "Key `xxy` not found")
+    }
+
+    const originalThrowOnMissingValue = c.options.errorOnMissing
+
+    c.options.errorOnMissing = true
+
+    try {
+      c.get("xxz")
+    } catch (error1) {
+      const err = error1
+      n++
+      t.equal(err.name, "ENOTFOUND")
+      // t.equal(err.message, "Key `xxz` not found")
+    }
+
+    c.options.errorOnMissing = originalThrowOnMissingValue
+
+    console.log(c.options.errorOnMissing)
+
+    c.del("xxx", function (err, res) {
+      n++
+      t.equal(err, null)
+      t.equal(0, res)
+
+    })
+
+    c.set(key, value2, 0, function (err, res) {
+      n++
+      t.equal(err, null)
+      t.ok(res, err)
+
+      c.get(key, function (err, res) {
+        n++
+        const pred = value2
+        t.equal(pred, res)
+        t.equal(1, c.getStats().keys - start.keys)
+      })
+    })
+
+    c.del(key, function (err, res) {
+      c.removeAllListeners("del")
+      n++
+      t.equal(err, null)
+      t.equal(1, res)
+      t.equal(0, c.getStats().keys - start.keys)
+
+      c.get(key, function (err, res) {
+        n++
+        t.equal(err, null)
+        t.equal(res, undefined)
+      })
+
+      c.set("zero", 0, 0, function (err, res) {
+        n++
+        t.equal(err, null)
+        t.ok(res, err)
+      })
+
+      c.get("zero", function (err, res) {
+        n++
+        t.equal(err, null)
+        t.equal(0, res)
+      })
+    })
+  })
+
+  if (typeof Promise !== "undefined" && Promise !== null) {
+    p = new Promise(function (fulfill, reject) {
+      return fulfill('Some deferred value')
+    })
+    p.then(function (value) {
+      t.equal(value, 'Some deferred value')
+    })
+    localCacheNoClone.set("promise", p)
+    q = localCacheNoClone.get("promise")
+    try {
+      q.then(function (value) {
+        n++;
+      })
+    } catch (error) {
+      _err = error;
+      t.ok(false, _err)
+      return;
+    }
+  } else {
+    console.log("No Promise test, because not availible in this node version")
+  }
+  // TODO
+  // beforeExit(function () {
+  //   var _count;
+  //   _count = 14;
+  //   if (typeof Promise !== "undefined" && Promise !== null) {
+  //     _count += 1;
+  //   }
+  //   t.equal(_count, n, "not exited")
+  // })
+})
+
+test('general sync', function (t) {
+  let res, pred
+  console.log("\nSTART GENERAL TEST SYNC")
+
+  const c = new VCache()
+
+  c.flushAll()
+
+  const start = _clone(c.getStats())
+  const value = randomString(100)
+  const value2 = randomString(100)
+  const key = randomString(10)
+
+  c.once("del", function (_key, _val) {
+    t.equal(_key, key)
+    t.equal(_val, value2)
+  })
+
+  t.ok(c.set(key, value, 0))
+  t.equal(1, c.getStats().keys - start.keys)
+
+  res = c.get(key)
+  t.equal(value, res)
+
+  res = c.keys()
+  pred = [key]
+  t.deepEqual(pred, res)
+
+  res = c.get("xxx")
+  t.equal(res, undefined)
+
+  res = c.del("xxx")
+  t.equal(0, res)
+
+  res = c.set(key, value2, 0)
+  t.ok(res, res)
+
+  res = c.get(key)
+  t.equal(value2, res)
+  t.equal(1, c.getStats().keys - start.keys)
+
+  res = c.del(key)
+  c.removeAllListeners("del")
+  t.equal(1, res)
+  t.equal(0, c.getStats().keys - start.keys)
+
+  res = c.get(key)
+  t.equal(res, undefined)
+
+  res = c.set("mulitA", 23)
+  t.ok(res, res)
+
+  res = c.set("mulitB", 23)
+  t.ok(res, res)
+
+  res = c.set("mulitC", 23)
+  t.ok(res, res)
+
+  res = c.get("mulitA")
+  t.equal(res, 23)
+
+  res = c.get("mulitB")
+  t.equal(res, 23)
+
+  res = c.get("mulitC")
+  t.equal(res, 23)
+
+  res = c.del(["mulitA", "mulitB"])
+  t.equal(2, res)
+
+  res = c.get("mulitA")
+  t.equal(res, undefined)
+
+  res = c.get("mulitB")
+  t.equal(res, undefined)
+
+  res = c.get("mulitC")
+  t.equal(res, 23)
+
+  res = c.del(["mulitC"])
+  t.equal(1, res)
+
+  res = c.get("mulitC")
+  t.equal(res, undefined)
+
+  res = c.del(["mulitA", "mulitB", "mulitC"])
+  t.equal(0, res)
+
+  res = c.set("zero", 0, 0)
+  t.ok(res, res)
+
+  res = c.get("zero")
+  t.equal(0, res)
+
+
+  let tObj = {
+    a: 1,
+    b: {
+      x: 2,
+      y: 3
+    }
+  }
+
+  res = c.set("clone", tObj, 0)
+  t.ok(res, res)
+  tObj.b.x = 666
+
+  res = c.get("clone")
+  t.equal(2, res.b.x)
+  res.b.y = 42
+
+  res2 = c.get("clone")
+  t.equal(3, res2.b.y)
+  t.end()
+})
+
+test('flush', function (t) {
+  t.plan(103)
+  var i, j, k, key, len, ref
+  console.log("\nSTART FLUSH TEST")
+
+  const c = new VCache()
+
+  let n = 0
+  const count = 100
+  const startKeys = c.getStats().keys
+  const ks = []
+  const val = randomString(20)
+
+  for (i = j = 1, ref = count; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+    key = randomString(7)
+    ks.push(key)
+  }
+
+  for (k = 0, len = ks.length; k < len; k++) {
+    key = ks[k]
+    c.set(key, val, 0, function (err, res) {
+      n++;
+      t.equal(err, null)
+    })
+  }
+  t.equal(c.getStats().keys, startKeys + count)
+  c.flushAll(false)
+  t.equal(c.getStats().keys, 0)
+  t.deepEqual(c.data, {})
+})
+
+
+test('many', function (t) {
+  t.plan(200000)
+
+  const c = new VCache()
+
+  let time, _dur, i, j, k, key, l, len, len1, ref;
+  let n = 0
+  const count = 100000
+  const ks = []
+
+  console.log("\nSTART MANY TEST/BENCHMARK.\nSet, Get and check " + count + " elements")
+
+  const val = randomString(20)
+
+  for (i = j = 1, ref = count; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+    key = randomString(7)
+    ks.push(key)
+  }
+  time = new Date().getTime()
+  for (k = 0, len = ks.length; k < len; k++) {
+    key = ks[k]
+    t.ok(c.set(key, val, 0))
+  }
+  _dur = new Date().getTime() - time
+
+  console.log("BENCHMARK for SET:", _dur + "ms", " ( " + (_dur / count) + "ms per item ) ")
+
+  time = new Date().getTime()
+  for (l = 0, len1 = ks.length; l < len1; l++) {
+    key = ks[l]
+    n++
+    t.equal(val, c.get(key))
+  }
+  _dur = new Date().getTime() - time
+
+  console.log("BENCHMARK for GET:", _dur + "ms", " ( " + (_dur / count) + "ms per item ) ")
+  console.log("BENCHMARK STATS:", c.getStats())
+})
+
+test('delete', function (t) {
+  console.log("\nSTART DELETE TEST")
+  const c = new VCache()
+
+  let n = 0
+  const count = 10000
+
+  const ks = [];
+  for (var i = 0; i < count; i++) {
+    let key = randomString(7)
+    ks.push(key)
+    c.set(key, {})
+  }
+
+  const startKeys = c.getStats().keys
+
+  for (var i = 0; i < count; i++) {
+    c.del(ks[i], function (err, res) {
+      n++
+      t.equal(err, null)
+      return t.equal(res, 1)
+    })
+  }
+
+  for (var i = 0; i < count; i++) {
+    c.del(ks[i], function (err, res) {
+      n++
+      t.equal(res, 0)
+      return t.equal(err, null)
+    })
+  }
+
+  t.equal(startKeys - count, c.getStats().keys)
+
+  t.end()
+})
+
+
+test('stats', function (t) {
+  t.plan(55)
+  var i, j, k, l, ref, ref1, ref2
+
+  const c = new VCache()
+
+  console.log("\nSTART STATS TEST")
+
+  let n = 0
+
+  const start = _clone(localCache.getStats())
+  const count = 5
+  const keys = []
+  const vals = []
+
+  for (i = j = 1, ref = count * 2; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+    let key = randomString(7)
+    let val = randomString(50)
+    keys.push(key)
+    vals.push(val)
+
+    c.set(key, val, 0, function (err, success) {
+      n++;
+      t.equal(err, null)
+      t.ok(success)
+    })
+  }
+
+  for (i = k = 1, ref1 = count; 1 <= ref1 ? k <= ref1 : k >= ref1; i = 1 <= ref1 ? ++k : --k) {
+    c.get(keys[i], function (err, res) {
+      n++
+      t.equal(vals[i], res)
+      t.equal(err, null)
+    })
+    c.del(keys[i], function (err, success) {
+      n++
+      t.equal(err, null)
+      t.ok(success)
+    })
+  }
+
+  for (i = l = 1, ref2 = count; 1 <= ref2 ? l <= ref2 : l >= ref2; i = 1 <= ref2 ? ++l : --l) {
+    c.get("xxxx", function (err, res) {
+      ++n;
+      t.equal(err, null)
+      t.equal(res, undefined)
+    })
+  }
+
+  const end = c.getStats()
+
+  t.equal(end.hits - start.hits, 5, "hits wrong")
+  t.equal(end.misses - start.misses, 5, "misses wrong")
+  t.equal(end.keys - start.keys, 5, "hits wrong")
+  t.equal(end.ksize - start.ksize, 5 * 7, "hits wrong")
+  t.equal(end.vsize - start.vsize, 5 * 50, "hits wrong")
+})
+
+
+test('multi', function (t) {
+  t.plan(110)
+  var i, j, k, l, len, len1, ref
+  console.log("\nSTART MULTI TEST")
+
+  const c = new VCache()
+
+  let n = 0
+  const count = 100;
+  const ks = [];
+  const val = randomString(20)
+
+  for (i = j = 1, ref = count; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+    let key = randomString(7)
+    ks.push(key)
+  }
+
+  for (k = 0, len = ks.length; k < len; k++) {
+    let key = ks[k]
+
+    c.set(key, val, 0, function (err, res) {
+      n++
+      t.equal(err, null)
+    })
+  }
+
+  const startKeys = c.getStats().keys
+  const getKeys = ks.splice(50, 5)
+
+  let pred = {}
+
+  for (l = 0, len1 = getKeys.length; l < len1; l++) {
+    key = getKeys[l]
+    pred[key] = val
+  }
+  c.mget(getKeys[0], function (err, res) {
+    n++
+    t.ok(err !== null)
+    t.equal(err.constructor.name, "Error")
+    t.equal("EKEYSTYPE", err.name)
+    t.equal(res, undefined)
+  })
+  c.mget(getKeys, function (err, res) {
+    n++
+    t.equal(err, null)
+    t.deepEqual(pred, res)
+  })
+  c.del(getKeys, function (err, res) {
+    n++
+    t.equal(err, null)
+    t.equal(getKeys.length, res)
+  })
+
+  c.mget(getKeys, function (err, res) {
+    n++
+    t.equal(err, null)
+    t.deepEqual(res, {})
+  })
+})
+
+test('ttl', function (t) {
+  t.plan(43)
+  console.log("\nSTART TTL TEST")
+
+  const c = new VCache({
+    stdTTL: 0
+  })
+
+  const val = randomString(20)
+  const key = "k1_" + randomString(7)
+  const key2 = "k2_" + randomString(7)
+  const key3 = "k3_" + randomString(7)
+  const key4 = "k4_" + randomString(7)
+  const key5 = "k5_" + randomString(7)
+  const _keys = [key, key2, key3, key4, key5]
+  let n = 0;
+  const _now = Date.now()
+
+  c.set(key, val, 0.5, function (err, res) {
+    t.equal(err, null)
+    t.ok(res)
+
+    const ts = c.getTTL(key)
+
+    if (ts > _now && ts < _now + 300) {
+      throw new Error("Invalid timestamp")
+    }
+
+    c.get(key, function (err, res) {
+      t.equal(err, null)
+      t.equal(val, res)
+    })
+  })
+
+  c.set(key2, val, 0.3, function (err, res) {
+    t.equal(err, null)
+    t.ok(res)
+
+    c.get(key2, function (err, res) {
+      t.equal(err, null)
+      t.equal(val, res)
+    })
+  })
+
+  setTimeout(function () {
+    ++n
+    c.get(key, function (err, res) {
+      t.equal(err, null)
+      t.deepEqual(val, res)
+    })
+  }, 400)
+
+  setTimeout(function () {
+    const ts = c.getTTL(key)
+    t.equal(ts, undefined)
+    ++n
+    c.get(key, function (err, res) {
+      t.equal(err, null)
+      t.equal(res, undefined)
+    })
+  }, 600)
+
+  setTimeout(function () {
+    ++n
+
+    c.getTTL(key, function (err, ts) {
+      if (ts > _now && ts < _now + 300) {
+        throw new Error("Invalid timestamp")
+      }
+    })
+
+    c.get(key2, function (err, res) {
+      t.equal(err, null)
+      t.equal(val, res)
+    })
+
+  }, 250)
+
+  setTimeout(function () {
+    process.nextTick(function () {
+      const startKeys = c.getStats().keys
+      const key = "autotest"
+      const _testExpired = function (_key, _val) {
+        if (Array.prototype.indexOf.call(_keys, _key) < 0) {
+          t.equal(_key, key)
+          t.equal(_val, val)
+        }
+      }
+
+      const _testSet = function (_key) {
+        t.equal(_key, key)
+      }
+
+      c.once("set", _testSet)
+      c.set(key, val, 0.5, function (err, res) {
+        t.equal(err, null)
+        t.ok(res)
+        t.equal(startKeys + 1, c.getStats().keys)
+
+        c.get(key, function (err, res) {
+          t.equal(val, res)
+          c.on("expired", _testExpired)
+
+          return setTimeout(function () {
+            c._checkData(false)
+            t.equal(c.data[key], undefined)
+            c.removeAllListeners("set")
+            c.removeAllListeners("expired")
+          }, 700)
+        })
+      })
+    })
+  }, 1000)
+
+  c.set(key3, val, 100, function (err, res) {
+    t.equal(err, null)
+    t.ok(res)
+    c.get(key3, function (err, res) {
+      t.equal(err, null)
+      t.equal(val, res)
+      c.ttl(key3 + "false", 0.3, function (err, setted) {
+        t.equal(err, null)
+        t.equal(false, setted)
+      })
+      c.ttl(key3, 0.3, function (err, setted) {
+        t.equal(err, null)
+        t.ok(setted)
+      })
+      c.get(key3, function (err, res) {
+        t.equal(val, res)
+      })
+      setTimeout(function () {
+        res = c.get(key3)
+        t.equal(res, undefined)
+        t.equal(c.data[key3], undefined)
+      }, 500)
+    })
+  })
+
+  c.set(key4, val, 100, function (err, res) {
+    t.equal(err, null)
+    t.ok(res)
+
+    c.get(key4, function (err, res) {
+      t.equal(err, null)
+      t.equal(val, res)
+      c.ttl(key4 + "false", function (err, setted) {
+        t.equal(err, null)
+        return t.equal(false, setted)
+      })
+      c.ttl(key4, function (err, setted) {
+        t.equal(err, null)
+        t.ok(setted)
+        t.equal(c.data[key4], undefined)
+      })
+    })
+  })
+})
+
+
+test('cache with TTL', function (t) {
+  t.plan(11)
+  const val = randomString(20)
+  const key5 = "k5_" + randomString(7)
+  localCacheTTL.set(key5, val, 100, function (err, res) {
+    t.equal(err, null)
+    t.ok(res)
+
+    localCacheTTL.get(key5, function (err, res) {
+      t.equal(err, null)
+      t.equal(val, res)
+
+      localCacheTTL.ttl(key5 + "false", function (err, setted) {
+        t.equal(err, null)
+        t.equal(false, setted)
+      })
+
+      localCacheTTL.ttl(key5, function (err, setted) {
+        t.equal(err, null)
+        t.ok(setted)
+      })
+
+      localCacheTTL.get(key5, function (err, res) {
+        t.equal(val, res)
+      })
+
+      setTimeout(function () {
+        let res = localCache.get(key5)
+        t.equal(res, undefined)
+
+        localCacheTTL._checkData(false)
+        t.equal(localCacheTTL.data[key5], undefined)
+      }, 500)
+    })
+  })
+})
