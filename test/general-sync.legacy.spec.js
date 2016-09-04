931471dfb4df2c701b17d6fa6b8815c0ee7121eb
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-09-04 16:43:00 +0200

    unimplement general sync test

--- test/general-sync.legacy.spec.js
+++ test/general-sync.legacy.spec.js
@@ -6,14 +6,6 @@ const _clone = require('./common')._clone
 const randomString = require('./common').randomString
 const c = new VCache()
 
-c.flushAll()
-
-const start = _clone(c.getStats())
-const value = randomString(100)
-const value2 = randomString(100)
-const key = randomString(10)
-
-
-test('general sync', function (t) {
+test('general: sync', function (t) {
   t.end()
 })
