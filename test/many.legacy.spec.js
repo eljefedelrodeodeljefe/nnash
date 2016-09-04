6858fb392f62aef0772a6f7748fa2858c4d23689
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2016-09-04 17:22:56 +0200

    make test many run

--- test/many.legacy.spec.js
+++ test/many.legacy.spec.js
@@ -7,42 +7,70 @@ const c = new VCache()
 const start = _clone(c.getStats())
 const value = randomString(100)
 const value2 = randomString(100)
-const key = randomString(10)
+
+const val = randomString(20)
+let n = 0
+
+function gen (ks, cb) {
+  for (var k = 0, len = ks.length; k < len; k++) {
+    let key = ks[k]
+
+    c.set(key, val, 0, (err) => {
+      assert.equal(err, null)
+
+      if (k === len - 1) {
+        return cb(null)
+      }
+    })
+  }
+}
+
+function check (ks, cb) {
+  for (var l = 0, len1 = ks.length; l < len1; l++) {
+    key = ks[l]
+    n++
+    c.get(key, (err, res) => {
+      if (err) {
+        return cb(err)
+      }
+      assert.equal(res, val)
+
+      if (l === len1 -1) {
+        return cb(null)
+      }
+    })
+  }
+}
 
 test('many', function (t) {
   const c = new VCache()
 
   let time, _dur, i, j, k, key, l, len, len1, ref
-  let n = 0
   const count = 100000
   const ks = []
 
   console.log('\nSTART MANY TEST/BENCHMARK.\nSet, Get and check ' + count + ' elements')
 
-  const val = randomString(20)
-
   for (i = j = 1, ref = count; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
     key = randomString(7)
     ks.push(key)
   }
-  time = new Date().getTime()
-  for (k = 0, len = ks.length; k < len; k++) {
-    key = ks[k]
-    assert.ok(c.set(key, val, 0))
-  }
-  _dur = new Date().getTime() - time
-
-  console.log('BENCHMARK for SET:', _dur + 'ms', ' ( ' + (_dur / count) + 'ms per item ) ')
 
   time = new Date().getTime()
-  for (l = 0, len1 = ks.length; l < len1; l++) {
-    key = ks[l]
-    n++
-    assert.equal(val, c.get(key))
-  }
-  _dur = new Date().getTime() - time
-  console.log('BENCHMARK for GET:', _dur + 'ms', ' ( ' + (_dur / count) + 'ms per item ) ')
-  console.log('BENCHMARK STATS:', c.getStats())
-  t.pass()
-  t.end()
+
+  gen(ks, () => {
+    _dur = new Date().getTime() - time
+
+    console.log('BENCHMARK for SET:', _dur + 'ms', ' ( ' + (_dur / count) + 'ms per item ) ')
+
+    time = new Date().getTime()
+
+    check(ks, () => {
+      _dur = new Date().getTime() - time
+      console.log('BENCHMARK for GET:', _dur + 'ms', ' ( ' + (_dur / count) + 'ms per item ) ')
+      console.log('BENCHMARK STATS:', c.getStats())
+      t.pass()
+      t.end()
+    })
+  })
 })
